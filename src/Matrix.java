b8785e277f9a5a75624d152781ed62beb417f92f
Lexi Ross <the.circ@gmail.com>
2012-03-24 16:18:15 -0400

    Finished implementation w/o testing.

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,18 +1,90 @@
+/*
+import java.util.List;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+*/
+import java.io.*;
+import java.util.ArrayList;
 
 public class Matrix {
 	public int[][] rows;
 	public int n;
 	
-	public Matrix(String filename) {
-		// TODO
-	}
+	
 	
 	public Matrix(int[][] rows) {
 		this.rows = rows;
 		this.n = rows[0].length;
 	}
 	
-	public void print() {
-		// TODO
+	public Matrix[] createFromFile(String filename, boolean firstHalf) {
+		int count = 0;
+		Matrix[] ret = new Matrix[2];
+		ArrayList<String> lines = new ArrayList<String>();
+		try {
+			FileReader input = new FileReader(filename);
+			BufferedReader reader = new BufferedReader(input);
+			String line = reader.readLine();
+			count++;
+			
+			while (line != null) {
+				lines.add(line);
+				line = reader.readLine();
+				count++;
+			}
+			
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String[] linesArray = (String[]) lines.toArray();
+		int d = count/2;
+		int n = (int) Math.sqrt(d);
+		int k = 0;
+		int l = 0;
+		for (int i = 0; i < 2; i++) {
+			int[][] rows = new int[n][n];
+			int[] row = new int[n];
+			k = 0;
+			l = 0;
+			for (int j = d*i; j < d*i+d; j++) {
+				row[k] = Integer.parseInt(linesArray[j]);
+				k++;
+				if (k == n) {
+					k = 0;
+					rows[l] = row;
+					l++;
+				}
+			}
+			ret[i] = new Matrix(rows);
+		}
+		return ret;
+	}
+	
+	/*
+	 * Prints a matrix to System.out.
+	 * If test is set to true, print all elements of the matrix. 
+	 * Else print only the diagonals.
+	 */
+	public void print(boolean test) {
+		if (test) {
+			for (int i = 0; i < this.n; i++) {
+				String line = "";
+				for (int j = 0; j < this.n; j++) {
+					line += this.rows[i][j] + "\t";
+				}
+				System.out.println(line);
+			}
+		} else {
+			for (int i = 0; i < this.n; i++) {
+				System.out.println(this.rows[i][i]);
+			}
+		}
+		System.out.println();
 	}
 }
--- src/Strassen.java
+++ src/Strassen.java
@@ -17,20 +17,46 @@ public class Strassen {
 	
 	/*
 	 * Adds/subtracts two matrices and returns the result. If the last
-	 * parameter is set to true, the second matrix will be subtracted
+	 * parameter is set to false, the second matrix will be subtracted
 	 * from the first.
 	 */
-	private Matrix add(Matrix m1, Matrix m2, boolean subtract) {
+	private Matrix add(Matrix m1, Matrix m2, boolean addition) {
 		// TODO
 		int n = m1.n;
 		int[][] ret = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (subtract) {
-					ret[i][j] = m1.rows[i][j] - m2.rows[i][j];
-				} else { 
+				if (addition) {
 					ret[i][j] = m1.rows[i][j] + m2.rows[i][j];
+				} else { 
+					ret[i][j] = m1.rows[i][j] - m2.rows[i][j];
 				}
+			}
+		}
+		return new Matrix(ret);
+	}
+	
+	private int[] concat(int[] a, int[] b) {
+		int[] c = new int[a.length+b.length];
+		System.arraycopy(a, 0, c, 0, a.length);
+		System.arraycopy(b, 0, c, a.length, b.length);
+		
+		return c;
+	}
+	
+	private Matrix combine(Matrix a, Matrix b, Matrix c, Matrix d) {
+		int half = a.n;		
+		int n = 2*half;
+		int[][] rowsA = a.rows;
+		int[][] rowsB = b.rows;
+		int[][] rowsC = c.rows;
+		int[][] rowsD = d.rows;
+		int[][] ret = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			if (i < half) {
+				ret[i] = concat(rowsA[i], rowsB[i]);
+			} else {
+				ret[i] = concat(rowsC[i-half], rowsD[i-half]);
 			}
 		}
 		return new Matrix(ret);
@@ -53,6 +79,62 @@ public class Strassen {
 			}
 		}
 		return new Matrix(ret);
+	}
+	
+	private Matrix strassenMultiply(Matrix m1, Matrix m2, int n, int crossover) {
+		if (n <= crossover) {
+			return conventionalMultiply(m1, m2);
+		} else {
+			Matrix[] matrices = {m1, m2};
+			
+			// divide both matrices into four submatrices
+			Matrix a,b,c,d,e,f,g,h;
+			int half = n/2;
+			int count = 0;
+			int[][][] submatrices = new int[8][half][half];
+			for (Matrix m : matrices) {
+				//TODO account for odd n
+				
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+						if (i < half) {
+							if (j < half) {
+								submatrices[0+4*count][i][j] = m.rows[i][j];
+							} else {
+								submatrices[1+4*count][i][j-half] = m.rows[i][j];
+							}
+						} else {
+							if (j < half) {
+								submatrices[2+4*count][i-half][j] = m.rows[i][j];
+							} else {
+								submatrices[3+4*count][i-half][j-half] = m.rows[i][j];
+							}
+						}
+					}
+				}
+				count++;
+			}
+			a = new Matrix(submatrices[0]);
+			b = new Matrix(submatrices[1]);
+			c = new Matrix(submatrices[2]);
+			d = new Matrix(submatrices[3]);
+			e = new Matrix(submatrices[4]);
+			f = new Matrix(submatrices[5]);
+			g = new Matrix(submatrices[6]);
+			h = new Matrix(submatrices[7]);
+			Matrix p1 = strassenMultiply(a, add(f,h,false), half, crossover);
+			Matrix p2 = strassenMultiply(add(a,b,true), h, half, crossover);
+			Matrix p3 = strassenMultiply(add(c,d,true), e, half, crossover);
+			Matrix p4 = strassenMultiply(d, add(g,e,false), half, crossover);
+			Matrix p5 = strassenMultiply(add(a,d,true), add(e,h,true), half, crossover);
+			Matrix p6 = strassenMultiply(add(b,d,false), add(g,h,true), half, crossover);
+			Matrix p7 = strassenMultiply(add(a,c,false), add(e,f,true), half, crossover);
+			Matrix topLeft = add(add(p5, add(p4,p2,false), true), p6, true);
+			Matrix topRight = add(p1,p2,true);
+			Matrix bottomLeft = add(p3,p4,true);
+			Matrix bottomRight = add(p5, add(p1, add(p3,p7,false), false), true);
+			return combine(topLeft, topRight, bottomLeft, bottomRight);
+		}
 	}
 
 	/**
